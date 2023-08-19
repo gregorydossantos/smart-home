4ebfb2b22e78d13b47085006004ef50a20baa850
Gregory Castezana <gregescribano@gmail.com>
2023-08-19 17:36:38 -0300

    Partial commit to API Address Register

--- src/main/java/com/fiap/gregory/smarthome/app/controllers/AddressRegisterController.java
+++ src/main/java/com/fiap/gregory/smarthome/app/controllers/AddressRegisterController.java
@@ -5,25 +5,41 @@ import com.fiap.gregory.smarthome.app.request.AddressRegisterRequest;
 import com.fiap.gregory.smarthome.app.services.AddressRegisterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/address-register")
 public class AddressRegisterController {
+
+    private static final String PATH_ID = "/{id}";
 
     @Autowired
     private AddressRegisterService service;
 
     @PostMapping
     public ResponseEntity<AddressRegisterDto> create(@RequestBody AddressRegisterRequest request) {
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-                .buildAndExpand(service.create(request)).toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().buildAndExpand(service.create(request)).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AddressRegisterDto>> readAll() {
+        return ResponseEntity.ok(service.read());
+    }
+
+    @PutMapping(PATH_ID)
+    public ResponseEntity<AddressRegisterDto> update(@PathVariable("id") Long id,
+                                                     @RequestBody AddressRegisterRequest request) {
+        var response = service.update(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping(PATH_ID)
+    public void delete(@PathVariable("id") Long id) {
+        service.delete(id);
     }
 }
--- src/main/java/com/fiap/gregory/smarthome/app/services/AddressRegisterService.java
+++ src/main/java/com/fiap/gregory/smarthome/app/services/AddressRegisterService.java
@@ -4,11 +4,18 @@ import com.fiap.gregory.smarthome.app.models.domains.AddressRegister;
 import com.fiap.gregory.smarthome.app.models.dtos.AddressRegisterDto;
 import com.fiap.gregory.smarthome.app.repositories.AddressRegisterRepository;
 import com.fiap.gregory.smarthome.app.request.AddressRegisterRequest;
+import com.fiap.gregory.smarthome.app.services.exceptions.BadRequestViolationException;
+import com.fiap.gregory.smarthome.app.services.exceptions.DataEmptyOrNullException;
 import com.fiap.gregory.smarthome.app.services.exceptions.DataIntegratyViolationException;
+import com.fiap.gregory.smarthome.app.useful.StringUseful;
 import com.fiap.gregory.smarthome.app.useful.ValidationUseful;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static com.fiap.gregory.smarthome.app.useful.StringUseful.isNullOrEmpty;
 
@@ -17,6 +24,8 @@ public class AddressRegisterService {
 
     private static final String ADDRESS_ALREADY_EXISTS = "Endereço já cadastrado!";
     private static final String BAD_REQUEST = "Erro no contexto da requisição!";
+    private static final String DATA_EMPTY_OR_NULL = "Nenhum endereço encontrado!";
+    private static final String REGISTER_NOT_FOUND = "Endereço inesxistente!";
 
     @Autowired
     private AddressRegisterRepository repository;
@@ -36,6 +45,40 @@ public class AddressRegisterService {
         return mapper.map(address, AddressRegisterDto.class);
     }
 
+    public List<AddressRegisterDto> read() {
+        var addressList = repository.findAll();
+
+        if (addressList.isEmpty()) {
+            throw new DataEmptyOrNullException(DATA_EMPTY_OR_NULL);
+        }
+
+        return addressList.stream().map(addressRegister -> mapper.map(addressRegister, AddressRegisterDto.class))
+                .collect(Collectors.toList());
+    }
+
+    public AddressRegisterDto update(Long id, AddressRegisterRequest request) {
+        validator.validateRequest(request);
+
+        var address = repository.findById(id);
+
+        var addressChange = updateAddress(address, request);
+
+        return mapper.map(addressChange, AddressRegisterDto.class);
+    }
+
+    public void delete(Long id) {
+        if (StringUseful.isNullOrEmpty(id)) {
+            throw new BadRequestViolationException(BAD_REQUEST);
+        }
+
+        var address = repository.findById(id);
+        if (address.isEmpty()) {
+            throw new DataEmptyOrNullException(DATA_EMPTY_OR_NULL);
+        }
+
+        repository.delete(address.get());
+    }
+
     private void findByStreetAndNumber(AddressRegisterRequest request) {
         var address = repository.findByStreetAndNumber(request.getStreet(), Integer.valueOf(request.getNumber()));
         if (!isNullOrEmpty(address)) {
@@ -51,5 +94,21 @@ public class AddressRegisterService {
                 .city(request.getCity())
                 .state(request.getState())
                 .build();
+    }
+
+    private AddressRegister updateAddress(Optional<AddressRegister> address, AddressRegisterRequest request) {
+        if (address.isEmpty()) {
+            throw new DataEmptyOrNullException(REGISTER_NOT_FOUND);
+        }
+
+        var addressChange = address.get();
+        addressChange.setStreet(request.getStreet());
+        addressChange.setNumber(Integer.parseInt(request.getNumber()));
+        addressChange.setDistrict(request.getDistrict());
+        addressChange.setCity(request.getCity());
+        addressChange.setState(addressChange.getState());
+        repository.saveAndFlush(addressChange);
+
+        return addressChange;
     }
 }
