78cfcadb595e7eecbe93cbe0a16c7e8b85a8809d
Gregory Castezana <gregescribano@gmail.com>
2023-09-12 18:04:42 -0300

    Add Swagger (openapi) in the project, to documentation APIs

--- src/main/java/com/fiap/gregory/smarthome/app/controllers/PeopleManagementController.java
+++ src/main/java/com/fiap/gregory/smarthome/app/controllers/PeopleManagementController.java
@@ -3,7 +3,12 @@ package com.fiap.gregory.smarthome.app.controllers;
 import com.fiap.gregory.smarthome.app.models.dtos.PeopleManagementDto;
 import com.fiap.gregory.smarthome.app.request.PeopleManagementRequest;
 import com.fiap.gregory.smarthome.app.services.PeopleManagementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -11,8 +16,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 
+@Tag(name = "People Controller")
 @RestController
-@RequestMapping(value = "/people")
+@RequestMapping(value = "/people", produces = {"application/json"})
 public class PeopleManagementController {
 
     private static final String PATH_ID = "/{id}";
@@ -20,24 +26,48 @@ public class PeopleManagementController {
     @Autowired
     private PeopleManagementService service;
 
-    @PostMapping
+    @Operation(summary = "Create a person", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successfully created person"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal error")
+    })
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PeopleManagementDto> create(@RequestBody PeopleManagementRequest request) {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .buildAndExpand(service.create(request)).toUri();
         return ResponseEntity.created(uri).build();
     }
 
+    @Operation(summary = "List of person", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Returns a list of people"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal error")
+    })
     @GetMapping
     public ResponseEntity<List<PeopleManagementDto>> readAll() {
         return ResponseEntity.ok(service.read());
     }
 
-    @PutMapping(PATH_ID)
+    @Operation(summary = "Update a person", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully update person"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal error")
+    })
+    @PutMapping(value = PATH_ID, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<PeopleManagementDto> update(@PathVariable("id") Long id,
                                                       @RequestBody PeopleManagementRequest request) {
         return ResponseEntity.ok(service.update(id, request));
     }
 
+    @Operation(summary = "Delete a person", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully delete a person"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "500", description = "Internal error")
+    })
     @DeleteMapping(PATH_ID)
     public void delete(@PathVariable("id") Long id) {
         service.delete(id);
